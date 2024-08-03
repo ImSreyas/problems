441a4df80e0f76ab3a9697b3f151e9ea3454582e
ImSreyas <sreyassatheesh755@gmail.com>
2024-08-03 11:03:53 +0530

    new python programmes added

--- python/longest_sub.py
+++ python/longest_sub.py
@@ -3,15 +3,29 @@ arr = []
 for i in range(n):
     arr.append(int(input()))
 
-max = 0
-for i in range(n-1):
-    count = 1
-    for j in range(len(arr[i+1:])):
-        if arr[i] < arr[j]:
-            count += 1
-        else:
-            break
-    if max < count:
-        max = count
+longest = 0
+count = 1
+for i in range(len(arr)-1):
+    if arr[i] < arr[i+1]:
+       count +=1 
+    else:
+        if longest < count:
+            longest = count
+            count = 1
 
-print("longest : ", max)
+print("longest is : ", max(longest, count))
+
+# max = 1
+# for i in range(n-1):
+#     count = 1
+#     val = arr[i]
+#     for j in range(i+1, n):
+#         if val < arr[j]:
+#             count += 1
+#             val = arr[j]
+#         else:
+#             break
+#     if max < count:
+#         max = count
+
+# print("longest : ", max)
